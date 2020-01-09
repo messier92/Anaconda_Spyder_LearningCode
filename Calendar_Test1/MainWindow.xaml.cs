04d89594c877ab7ad90e755dc6f70c1cf7df9695
Han Long Eugene, Goh <hanlong2102@hotmail.com>
2020-01-10 07:12:46 +0800

    Add files via upload

---
+++ Calendar_Test1/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calendar_Test1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<string> months = new List<string> { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+            cboMonth.ItemsSource = months;
+
+            for (int i = -50; i < 50; i++)
+            {
+                cboYear.Items.Add(DateTime.Today.AddYears(i).Year);
+            }
+
+            cboMonth.SelectedItem = months.FirstOrDefault(w => w == DateTime.Today.ToString("MMMM"));
+            cboYear.SelectedItem = DateTime.Today.Year;
+
+            cboMonth.SelectionChanged += (o, e) => RefreshCalendar();
+            cboYear.SelectionChanged += (o, e) => RefreshCalendar();
+        }
+
+        private void RefreshCalendar()
+        {
+            if (cboYear.SelectedItem == null) return;
+            if (cboMonth.SelectedItem == null) return;
+
+            int year = (int)cboYear.SelectedItem;
+
+            int month = cboMonth.SelectedIndex + 1;
+
+            DateTime targetDate = new DateTime(year, month, 1);
+
+            Calendar.BuildCalendar(targetDate);
+        }
+
+        private void Calendar_DayChanged(object sender, DayChangedEventArgs e)
+        {
+            //save the text edits to persistant storage
+        }
+
+
+
+
+    }
+}
